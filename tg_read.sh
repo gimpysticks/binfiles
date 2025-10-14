e149fa4ea764df2027ebfb63337573dce25c3694
John Sticks Kilbane <gimpysticks@gmail.com>
2025-10-14 11:31:08 -0400

    Removed files that contained tokens

--- tg_read.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-/usr/bin/tmux kill-session tg_mark_read
-/usr/bin/tmux new -d -s tg_mark_read
-/usr/bin/tmux split-window -v
-/usr/bin/tmux send -t tg_mark_read "telegram-cli --json -P 4458" ENTER
-/usr/bin/tmux select-pane -t 0
-sleep 3
-/usr/bin/tmux send -t tg_mark_read "/home/sticks/mark_read/mark_read.py" ENTER
-/usr/bin/tmux a
